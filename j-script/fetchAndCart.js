0e0fd6e20774e88d8798614c2e74fae3a740559b
3almsimsim <139587020+3almsimsim@users.noreply.github.com>
2024-07-22 13:40:13 +0300

    Update fetchAndCart.js

--- j-script/fetchAndCart.js
+++ j-script/fetchAndCart.js
@@ -56,7 +56,7 @@ class UI {
       if(product.section === "new"){
         result += `
      <div class="week-prod product-box">
-          <a href=""><img src=${product.img} alt="" class="product-img"></a>
+          <a class="product-img" href=""><img src=${product.img} alt="" class="product-img"></a>
           <a href="#" class="new-p-link">${product.desc}</a>
           <div class="star">
             <i class="fas fa-star"></i>
@@ -78,7 +78,7 @@ class UI {
       if(product.section != "new"){
         result += `
         <div class="week-prod product-box">
-        <a href=""><img src=${product.img} alt="" class="product-img"></a>
+        <a class="product-img" href=""><img src=${product.img} alt="" class="product-img"></a>
         <a href="#" class="new-p-link">${product.desc}</a>
         <div class="star">
         <i class="fas fa-star"></i>
